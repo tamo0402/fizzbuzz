555825bcb93984519bc947ae5ba9a28f007d88d2
m-tamoto <m-tamoto@bravetact.com>
2019-06-26 12:11:53 +0900

    5回目の変更

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,16 +1,8 @@
 <?php
 namespace CodeIQ;
 
-1回目の変更
-2回目の変更
 require_once 'FizzBuzzSpecification.php';
 require_once 'FizzBuzzApplication.php';
 
-$app = new FizzBuzzApplication();
-$app->addSpecAndMessage(new FizzBuzzSpecification(15), 'fizzbuzz');
-$app->addSpecAndMessage(new FizzBuzzSpecification(3), 'fizz');
-$app->addSpecAndMessage(new FizzBuzzSpecification(5), 'buzz');
 
-$data = range(1,30);
-
-$app->run($data);
+5回目の変更
